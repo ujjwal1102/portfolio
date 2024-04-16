efa566af06b291ae626909280a087ee3609b27b9
Coderboy <srivastavaaman2017@gmail.com>
2024-04-16 08:07:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import MainPage from './pages/home.jsx'
+import '../src/assets/css/style.css'
+import '../src/assets/vendor/bootstrap/css/bootstrap.min.css'
+import '../src/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '../src/assets/vendor/boxicons/css/boxicons.min.css'
+import '../src/assets/vendor/glightbox/css/glightbox.min.css'
+import '../src/assets/vendor/swiper/swiper-bundle.min.css'
+import '../src/assets/js/main.js'
+
+function App() {
+  return (
+    <>
+      <MainPage/>
+    </>
+  )
+}
+
+export default App
